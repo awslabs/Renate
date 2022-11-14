260e5433bc1e939dcc2d6cffd41d2773bd0131d9
Martin Wistuba <marwistu@amazon.com>
2022-11-14 18:13:39 +0100

    update examples

--- examples/simple_classifier_cifar10/start_with_hpo.py
+++ examples/simple_classifier_cifar10/start_with_hpo.py
@@ -17,7 +17,6 @@ config_space = {
     "batch_size": choice([32, 64, 128, 256]),
     "memory_batch_size": 32,
     "memory_size": 1000,
-    "max_epochs": 50,
     "loss_normalization": 0,
     "loss_weight": uniform(0.0, 1.0),
 }
@@ -29,6 +28,7 @@ if __name__ == "__main__":
         mode="max",
         metric="val_accuracy",
         updater="ER",  # we train with Experience Replay
+        max_epochs=50,
         chunk_id=0,  # we select the first chunk of our dataset, you will probably not need this in practice
         model_data_definition="./split_cifar10.py",
         requirements_file=str(Path(renate.__path__[0]).resolve().parents[1] / "requirements.txt"),
