02a44446d63d9a1c8595dcae4607cbbea99e7c00
Martin Wistuba <marwistu@amazon.com>
2022-11-21 11:11:32 +0100

    add all scenarios and mlp args

--- src/renate/benchmark/experimentation_config.py
+++ src/renate/benchmark/experimentation_config.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import ast
 from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import torch
 import torchmetrics
@@ -27,13 +27,24 @@ from renate.benchmark.models.vision_transformer import (
     VisionTransformerL16,
     VisionTransformerL32,
 )
-from renate.benchmark.scenarios import BenchmarkScenario, ClassIncrementalScenario, Scenario
+from renate.benchmark.scenarios import (
+    BenchmarkScenario,
+    ClassIncrementalScenario,
+    ImageRotationScenario,
+    PermutationScenario,
+    Scenario,
+)
 from renate.data.data_module import RenateDataModule
 from renate.models import RenateModule
 
 
 def model_fn(
-    model_state_url: Optional[Union[Path, str]] = None, model_fn_model_name: Optional[str] = None
+    model_state_url: Optional[Union[Path, str]] = None,
+    model_fn_model_name: Optional[str] = None,
+    model_fn_num_inputs: Optional[str] = None,
+    model_fn_num_outputs: Optional[str] = None,
+    model_fn_num_hidden_layers: Optional[str] = None,
+    model_fn_hidden_size: Optional[str] = None,
 ) -> RenateModule:
     """Returns a model instance."""
     models = {
@@ -53,8 +64,16 @@ def model_fn(
     }
     assert model_fn_model_name in models, f"Unknown model: {model_fn_model_name}"
     model_class = models[model_fn_model_name]
+    model_kwargs = {}
+    if model_fn_model_name == "MultiLayerPerceptron":
+        model_kwargs = {
+            "num_inputs": int(model_fn_num_inputs),
+            "num_outputs": int(model_fn_num_outputs),
+            "num_hidden_layers": int(model_fn_num_hidden_layers),
+            "hidden_size": ast.literal_eval(model_fn_hidden_size),
+        }
     if model_state_url is None:
-        model = model_class()
+        model = model_class(**model_kwargs)
     else:
         state_dict = torch.load(str(model_state_url))
         model = model_class.from_state_dict(state_dict)
@@ -84,7 +103,27 @@ def get_scenario(
     seed: int,
     num_tasks: Optional[int] = None,
     class_groupings: Optional[List[List[int]]] = None,
+    degrees: Optional[List[float]] = None,
+    input_dim: Optional[Union[List[int], Tuple[int], int]] = None,
 ) -> Scenario:
+    """Function to create scenario based on name and arguments.
+
+    Args:
+        scenario_name: Name to identify scenario.
+        data_module: The base data module.
+        chunk_id: The data chunk to load in for the training or validation data.
+        seed: A random seed to fix the created scenario.
+        num_tasks: The total number of expected tasks for experimentation.
+        class_groupings: Used for scenario `class_incremental`. Partitions classes into different chunks
+        degrees: Used for scenario `rotation`. Rotations applied for each chunk.
+        input_dim: Used for scenario `permutation`. Input dimensionality.
+
+    Returns:
+        An instance of the requested scenario.
+
+    Raises:
+        ValueError: If scenario name is unknown.
+    """
     if scenario_name == "class_incremental":
         assert (
             class_groupings is not None
@@ -98,7 +137,18 @@ def get_scenario(
         return BenchmarkScenario(
             data_module=data_module, num_tasks=num_tasks, chunk_id=chunk_id, seed=seed
         )
-    # TODO:
+    if scenario_name == "rotation":
+        return ImageRotationScenario(
+            data_module=data_module, degrees=degrees, chunk_id=chunk_id, seed=seed
+        )
+    if scenario_name == "permutation":
+        return PermutationScenario(
+            data_module=data_module,
+            num_tasks=num_tasks,
+            input_dim=input_dim,
+            chunk_id=chunk_id,
+            seed=seed,
+        )
     raise ValueError(f"Unknown scenario `{scenario_name}`.")
 
 
@@ -110,6 +160,8 @@ def data_module_fn(
     data_module_fn_dataset_name: Optional[str] = None,
     data_module_fn_val_size: str = "0.0",
     data_module_fn_class_groupings: Optional[str] = None,
+    data_module_fn_degrees: Optional[str] = None,
+    data_module_fn_input_dim: Optional[str] = None,
 ):
     data_module = get_data_module(
         data_path=str(data_path),
@@ -123,6 +175,8 @@ def data_module_fn(
         chunk_id=chunk_id,
         seed=seed,
         class_groupings=ast.literal_eval(data_module_fn_class_groupings),
+        degrees=ast.literal_eval(data_module_fn_degrees),
+        input_dim=ast.literal_eval(data_module_fn_input_dim),
     )
 
 
