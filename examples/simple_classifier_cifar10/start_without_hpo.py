96336d35d7d0ca3fee1e9ee97af38117b529d2a0
Giovanni Zappella <zappella@88665a4bd234.ant.amazon.com>
2022-11-21 14:43:15 +0100

    cifar10 example WIP

--- examples/simple_classifier_cifar10/start_without_hpo.py
+++ examples/simple_classifier_cifar10/start_without_hpo.py
@@ -1,10 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-from pathlib import Path
 
 from syne_tune.backend.sagemaker_backend.sagemaker_utils import get_execution_role
 
-import renate
 from renate.tuning import execute_tuning_job
 
 config_space = {
@@ -22,15 +20,19 @@ config_space = {
 
 if __name__ == "__main__":
 
+    AWSID = "387922178948"
+    AWSREGION = "us-west-2"
+
     execute_tuning_job(
         config_space=config_space,
         mode="max",
         metric="val_accuracy",
         updater="ER",
         max_epochs=50,
-        chunk_id=1,
-        config_file="split_cifar10.py",
-        requirements_file=str(Path(renate.__path__[0]).resolve().parents[1] / "requirements.txt"),
+        chunk_id=0,
+        config_file="renate_config.py",
+        requirements_file="requirements.txt",
+        next_state_url=f"s3://sagemaker-{AWSREGION}-{AWSID}/renate-training-test-cifar10/",
         backend="sagemaker",
         role=get_execution_role(),
         instance_type="ml.g4dn.2xlarge",
