21fd80431cc2771c439fd465dbd2d83cfb10115d
Martin Wistuba <marwistu@amazon.com>
2022-11-24 17:38:30 +0100

    step1

--- test/renate/benchmark/test_experimentation.py
+++ test/renate/benchmark/test_experimentation.py
@@ -2,18 +2,40 @@
 # SPDX-License-Identifier: Apache-2.0
 from pathlib import Path
 
+import pytest
+
 from renate.benchmark.experimentation import execute_experiment_job
 
 
-def test_execute_experiment_job(tmpdir):
-    execute_experiment_job(
-        backend="local",
-        config_file=str(Path(__file__).parent.parent / "renate_config_files" / "config.py"),
-        config_space={"updater": "ER", "data_module_fn_use_scenario": "True", "max_epochs": 5},
-        experiment_outputs_url=tmpdir,
-        mode="max",
-        metric="val_accuracy",
-        num_updates=2,
-        max_time=15,
-        seed=0,
-    )
+@pytest.fixture
+def experiment_job_kwargs():
+    return {
+        "backend": "local",
+        "config_file": str(Path(__file__).parent.parent / "renate_config_files" / "config.py"),
+        "config_space": {"updater": "ER", "data_module_fn_use_scenario": "True", "max_epochs": 5},
+        "mode": "max",
+        "metric": "val_accuracy",
+        "num_updates": 2,
+        "max_time": 15,
+        "seed": 0,
+    }
+
+
+def test_execute_experiment_job(tmpdir, experiment_job_kwargs):
+    """Only checking if things run, not testing anything besides that."""
+    execute_experiment_job(experiment_outputs_url=tmpdir, **experiment_job_kwargs)
+
+
+@pytest.mark.parametrize(
+    "update_dict,regex",
+    [
+        ({"backend": "UNKNOWN_BACKEND"}, r"Backend*"),
+        ({"mode": "UNKNOWN_MODE"}, r"Mode*"),
+        ({"num_updates": 3}, r"The dataset has*"),
+    ],
+)
+def test_execute_experiment_job_edge_cases(tmpdir, experiment_job_kwargs, update_dict, regex):
+    """Check if basic input errors raise an exception."""
+    experiment_job_kwargs.update(update_dict)
+    with pytest.raises(AssertionError, match=regex):
+        execute_experiment_job(experiment_outputs_url=tmpdir, **experiment_job_kwargs)
