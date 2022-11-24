5f40c01763f63029f05f9010631811f5fc510fe4
Giovanni <52964960+610v4nn1@users.noreply.github.com>
2022-11-24 18:58:12 +0100

    Add high level doc on how to run training (#38)
    Co-authored-by: Martin Wistuba <marwistu@amazon.com>

---
+++ examples/train_mlp_locally/start_training_with_er_without_hpo.py
@@ -0,0 +1,26 @@
+from renate.tuning import execute_tuning_job
+
+configuration = {
+    "optimizer": "SGD",
+    "momentum": 0.0,
+    "weight_decay": 1e-2,
+    "learning_rate": 0.05,
+    "batch_size": 32,
+    "max_epochs": 50,
+    "memory_batch_size": 32,
+    "memory_size": 500,
+}
+
+if __name__ == "__main__":
+
+    execute_tuning_job(
+        config_space=configuration,
+        mode="max",
+        metric="val_accuracy",
+        updater="ER",
+        max_epochs=50,
+        chunk_id=0,
+        config_file="renate_config.py",
+        next_state_url="./output_folder/",  # this is where the model will be stored
+        backend="local",  # the training job will run on the local machine
+    )
