70fd340a24d7471dbd67cc872b596211fcf20c50
Lukas Balles <balleslb@amazon.com>
2022-11-25 09:33:44 +0100

    More values for loss_weight_new_data in default config space

--- src/renate/tuning/config_spaces.py
+++ src/renate/tuning/config_spaces.py
@@ -59,9 +59,7 @@ _super_er_config_space = {
 _repeated_distill_config_space = _replay_config_space
 _offline_er_config_space = {
     **_replay_config_space,
-    **{
-        "loss_weight_new_data": choice([None, 0.5])
-    }
+    **{"loss_weight_new_data": choice([None, 0.25, 0.5, 0.75])},
 }
 
 
