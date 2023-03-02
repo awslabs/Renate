5ed8231e529610f0c8fc19cfbc49df915e1eb956
Lukas Balles <balleslb@amazon.com>
2023-03-02 15:41:48 +0100

    Fix buffer test

--- test/renate/memory/test_buffer_v2.py
+++ test/renate/memory/test_buffer_v2.py
@@ -21,7 +21,7 @@ def nested_tensors_equal(t1, t2):
         print("Type mismatch")
         return False
     if isinstance(t1, torch.Tensor):
-        return torch.allclose(t1, t2)
+        return torch.allclose(t1, t2, rtol=1e-3)
     if isinstance(t1, tuple):
         if len(t1) != len(t2):
             print("Tuple length mismatch")
