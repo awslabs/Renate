4eb54ee78cca0bcc6fae6c4fd51154db9449163c
Lukas Balles <balleslb@amazon.com>
2023-03-24 09:45:30 +0100

    Remove unused import

--- examples/nlp_finetuning/renate_config.py
+++ examples/nlp_finetuning/renate_config.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 from pathlib import Path
-from typing import Callable, Optional, Union
+from typing import Optional, Union
 
 import torch
 import transformers
