8a860c8d1bb94bdc80b0b675447545f48f1bf481
Martin Wistuba <marwistu@amazon.com>
2023-03-28 16:39:37 +0200

    flake

--- test/integration_tests/run_experiment.py
+++ test/integration_tests/run_experiment.py
@@ -70,7 +70,10 @@ if __name__ == "__main__":
         role = None
     else:
         AWS_ACCOUNT_ID = boto3.client("sts").get_caller_identity().get("Account")
-        experiment_outputs_url = f"s3://sagemaker-us-west-2-{AWS_ACCOUNT_ID}/renate-integration-tests/{args.test_suite}/{args.job_name}/"
+        experiment_outputs_url = (
+            f"s3://sagemaker-us-west-2-{AWS_ACCOUNT_ID}/renate-integration-tests/"
+            f"{args.test_suite}/{args.job_name}/"
+        )
         role = get_execution_role()
     execute_experiment_job(
         backend=args.backend,
