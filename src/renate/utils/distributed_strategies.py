67ac54c8755a0b7cfcc48ae48a393f4e6bd28834
Prabhu Teja <prbuteja@amazon.de>
2023-04-24 14:26:07 +0200

    strategy maker with license

--- src/renate/utils/distributed_strategies.py
+++ src/renate/utils/distributed_strategies.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import warnings
 from typing import Optional
 from pytorch_lightning.strategies import Strategy, StrategyRegistry
