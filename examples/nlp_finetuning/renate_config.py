eb4197fd89e88b0d1f9cb8437648f4502e98873e
Martin Wistuba <marwistu@amazon.com>
2023-05-03 16:20:16 +0200

    add transformer and stuff to benchmark config

--- examples/nlp_finetuning/renate_config.py
+++ examples/nlp_finetuning/renate_config.py
@@ -1,7 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-from pathlib import Path
-from typing import Optional, Union
+from typing import Optional
 
 import torch
 import transformers
@@ -13,26 +12,24 @@ from renate.models import RenateModule
 from renate.models.renate_module import RenateWrapper
 
 
-def model_fn(model_state_url: Optional[Union[Path, str]] = None) -> RenateModule:
+def model_fn(model_state_url: Optional[str] = None) -> RenateModule:
     """Returns a DistilBert classification model."""
     transformer_model = transformers.DistilBertForSequenceClassification.from_pretrained(
         "distilbert-base-uncased", num_labels=2, return_dict=False
     )
     model = RenateWrapper(transformer_model, loss_fn=torch.nn.CrossEntropyLoss())
     if model_state_url is not None:
-        state_dict = torch.load(str(model_state_url))
+        state_dict = torch.load(model_state_url)
         model.load_state_dict(state_dict)
     return model
 
 
-def data_module_fn(
-    data_path: Union[Path, str], chunk_id: int, seed: int = defaults.SEED
-) -> RenateDataModule:
+def data_module_fn(data_path: str, chunk_id: int, seed: int = defaults.SEED) -> RenateDataModule:
     """Returns one of two movie review datasets depending on `chunk_id`."""
     tokenizer = transformers.DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
     dataset_name = "imdb" if chunk_id else "rotten_tomatoes"
     data_module = HuggingfaceTextDataModule(
-        str(data_path),
+        data_path,
         dataset_name=dataset_name,
         tokenizer=tokenizer,
         val_size=0.2,
