d83374fefe3664b4942d2bdf009dd8045d53b4fa
Prabhu Teja <prbuteja@amazon.de>
2023-05-04 18:06:25 +0000

    utils cleanup

--- src/renate/utils/misc.py
+++ src/renate/utils/misc.py
@@ -1,11 +1,13 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from typing import Union
 from pathlib import Path
 
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
 
-def int_or_str(x: str)-> Union[str, int]:
-    """ Function to cast to int or str. This is used to tackle precision 
+def int_or_str(x: str) -> Union[str, int]:
+    """Function to cast to int or str. This is used to tackle precision
     which can be int (16, 32) or str (bf16)"""
     try:
         return int(x)
@@ -15,7 +17,7 @@ def int_or_str(x: str)-> Union[str, int]:
 
 @rank_zero_only
 def unlink_file_or_folder(path: Path) -> None:
-    """ Funtion to remove files and folders. Unlink works for files, rmdir
+    """Funtion to remove files and folders. Unlink works for files, rmdir
     for empty folders, but not for non-empty ones. Hence a recursive solution.
     """
     if path.exists():
