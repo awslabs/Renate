359a7c74e5181a9166c0ebeb6889cbdf84c80ac1
Prabhu Teja <prbuteja@amazon.de>
2023-05-22 19:31:50 +0200

    reducing max_time

--- test/renate/benchmark/test_experimentation.py
+++ test/renate/benchmark/test_experimentation.py
@@ -19,7 +19,7 @@ def experiment_job_kwargs():
         "mode": "max",
         "metric": "val_accuracy",
         "num_updates": 2,
-        "max_time": 360,
+        "max_time": 15,
         "seed": 0,
         "accelerator": "cpu",
         "devices": 1,
