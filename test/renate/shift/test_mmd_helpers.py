10fdf8ad619a487fd46498187cc78eb662a074c6
Lukas Balles <balleslb@amazon.com>
2023-05-22 14:50:40 +0200

    Remove unused import

--- test/renate/shift/test_mmd_helpers.py
+++ test/renate/shift/test_mmd_helpers.py
@@ -1,6 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-import pytest
 import torch
 
 from renate.shift.kernels import RBFKernel
