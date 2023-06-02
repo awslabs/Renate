2bf2f26c7226b01d29a398bf76b6530a20511219
Prabhu Teja <prbuteja@amazon.de>
2023-06-02 14:30:55 +0200

    updating LocalBackend arguments for SyneTune

--- src/renate/training/training.py
+++ src/renate/training/training.py
@@ -562,9 +562,8 @@ def _execute_training_and_tuning_job_locally(
             f"Tuning hyperparameters with respect to {metric} ({mode}) for {max_time} seconds on "
             f"{n_workers} worker(s)."
         )
-    # TODO: After bumping up SyneTune >= 0.6, use the argument `num_gpus_per_trial`.
 
-    backend = LocalBackend(entry_point=training_script, rotate_gpus=False if devices > 1 else True)
+    backend = LocalBackend(entry_point=training_script, num_gpus_per_trial=devices)
     if scheduler is None or not tune_hyperparameters:
         if scheduler is not None:
             warnings.warn(
