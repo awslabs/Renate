7bc816bb5e105b1400e87bbdaad5b05c2a22aed7
Giovanni Zappella <zappella@88665a4bd234.ant.amazon.com>
2023-07-27 11:44:51 +0200

    Add quick test

--- test/renate/benchmark/datasets/test_multi_data_nlp.py
+++ test/renate/benchmark/datasets/test_multi_data_nlp.py
@@ -5,6 +5,25 @@ import transformers as transformers
 from renate.benchmark.datasets.nlp_datasets import MultiTextDataModule
 
 
+def test_multi_data_nlp_small():
+    TRAIN_SIZE = 100
+    TEST_SIZE = 100
+
+    data = MultiTextDataModule(
+        "./remove_folder/",
+        train_size=TRAIN_SIZE,
+        test_size=TEST_SIZE,
+        domain="ag_news",
+        tokenizer=transformers.DistilBertTokenizer.from_pretrained("distilbert-base-uncased"),
+    )
+
+    data.prepare_data()
+    data.setup()
+
+    assert len(data.train_data()) == TRAIN_SIZE
+    assert len(data.test_data()) == TEST_SIZE
+
+
 # @pytest.mark.skip(reason="This test requires downloading and processing five datasets.")
 def test_multi_data_nlp_full():
     TRAIN_SIZE = 115000
