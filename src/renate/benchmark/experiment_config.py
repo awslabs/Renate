f94379c2f6dbc21eea0358ef2be8411841452b6d
Martin Wistuba <marwistu@amazon.com>
2023-08-10 17:43:59 +0200

    remove normalize

--- src/renate/benchmark/experiment_config.py
+++ src/renate/benchmark/experiment_config.py
@@ -334,7 +334,6 @@ def train_transform(dataset_name: str, model_name: Optional[str] = None) -> Opti
                     transforms.Resize(224),
                     transforms.RandomHorizontalFlip(),
                     default_transform(dataset_name),
-                    transforms.Normalize(0.5055697, 0.27123657),
                     transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
                 ]
             )
