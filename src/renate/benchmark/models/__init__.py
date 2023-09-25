93bd6b0b5cf518b6082a5758fc5049f05f27b770
Prabhu Teja <prabhuteja12@gmail.com>
2023-09-25 11:08:48 +0200

    fixing missing prompt

--- src/renate/benchmark/models/__init__.py
+++ src/renate/benchmark/models/__init__.py
@@ -9,7 +9,7 @@ from renate.benchmark.models.resnet import (
     ResNet50,
     ResNet50CIFAR,
 )
-from renate.benchmark.models.l2p import LearningToPromptTransformer
+from renate.benchmark.models.l2p import LearningToPromptTransformer, PromptedTransformer
 from renate.benchmark.models.spromptmodel import SPromptTransformer
 from renate.benchmark.models.vision_transformer import (
     VisionTransformerB16,
@@ -29,6 +29,7 @@ __all__ = [
     "ResNet50",
     "ResNet50CIFAR",
     "LearningToPromptTransformer",
+    "PromptedTransformer",
     "SPromptTransformer",
     "VisionTransformerB16",
     "VisionTransformerB32",
